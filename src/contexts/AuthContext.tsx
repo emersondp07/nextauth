c31b2d735321abd2b8b215a27eb433780451efcf
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2023-09-19 21:13:38 -0300

    initial project nextauth

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,62 @@
+import Router from "next/router";
+import { ReactNode, createContext, useState } from "react";
+import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User | undefined;
+  isAuthenticated: boolean;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const { data } = await api.post("sessions", {
+        email,
+        password,
+      });
+
+      const { token, refreshToken, permissions, roles } = data;
+
+      // sessionStorage
+      // localStorage
+      // cookies
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      Router.push("dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
